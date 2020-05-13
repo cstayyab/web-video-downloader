7b038c5c07341c599c060b19e4068ea602b7ca4c
cstayyab <cstayyab@gmail.com>
2020-05-13 22:12:30 +0500

    Added Status Code

--- api/fb.js
+++ api/fb.js
@@ -2,17 +2,17 @@ import getInfo from "fb-video-downloader";
 
 export default async (req, res) => {
   if(!req.body.videoId) {
-    res.json({
+    res.status(200).json({
       error: "Video ID not supplied"
     });
     return;
   }
   if(!req.body.user) {
-    res.json({
+    res.status(200).json({
       error: "Username not supplied"
     });
     return;
   }
   const v = await getInfo(`https://www.facebook.com/${req.body.user}/videos/${req.body.videoId}/`);
-  res.json(v);
+  res.status(200).json(v);
 };
