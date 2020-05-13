dbc71e33879a1ca18b0c594536ff3d40ef798997
cstayyab <cstayyab@gmail.com>
2020-05-14 04:05:15 +0500

    Removed Extra Code and Libraries

--- api/fb.js
+++ api/fb.js
@@ -1,5 +1,4 @@
 import  fbvid from 'fbvideos';
-import validURL from 'valid-url';
 
 module.exports =  async (req, res) => {
   if(!req.query.videoId) {
@@ -12,25 +11,20 @@ module.exports =  async (req, res) => {
       error: "Username not supplied"
     });
   }else {
-    //res.json({online: true, videoId: req.query.videoId, user: req.query.user});
     const video = `https://www.facebook.com/${req.query.user}/videos/${req.query.videoId}/`;
     const low = await fbvid.low(video);
     const high = await fbvid.high(video);
     var response = {}
-    if(validURL.isUri(low)) {
+    if((typeof low) === "object") {
       response.low = low;
     } else {
-      response.lowError = low;
+      response.low = {error: low};
     }
-    if(validURL.isUri(high)) {
+    if((typeof high) === "object") {
       response.high = high;
     } else {
-      response.highError = high;
+      response.high = {error: high};
     }
     res.json(response);
   }
-  
-  // const v = {success: true }//await getInfo(`https://www.facebook.com/${req.body.user}/videos/${req.body.videoId}/`);
-  // res.status(200).json(v);
-  // return 200;
 };
