82a6dc19ac58a9490a8325a0bf7ba2d339f4b9e1
cstayyab <cstayyab@gmail.com>
2020-05-13 22:48:16 +0500

    Added User Parameter

--- api/fb.js
+++ api/fb.js
@@ -6,15 +6,14 @@ module.exports =  async (req, res) => {
       error: "Video ID not supplied"
     });
 
-  } else {
-    res.json({online: true, videoId: req.query.videoId});
+  } else if(!req.query.user) {
+    res.status(200).json({
+      error: "Username not supplied"
+    });
+  }else {
+    res.json({online: true, videoId: req.query.videoId, user: req.query.user});
   }
-  // if(!req.body.user) {
-  //   res.status(200).json({
-  //     error: "Username not supplied"
-  //   });
-  //   return 200;
-  // }
+  
   // const v = {success: true }//await getInfo(`https://www.facebook.com/${req.body.user}/videos/${req.body.videoId}/`);
   // res.status(200).json(v);
   // return 200;
