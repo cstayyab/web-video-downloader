e79b24eb65a906998b671b8ad396146f35cc80ec
cstayyab <cstayyab@gmail.com>
2020-05-14 18:55:23 +0500

    Updated v2 of API

---
+++ api/v2/fb.js
@@ -0,0 +1,45 @@
+var fbvid = require('fbvideos');
+var pageMetadataParser = require("page-metadata-parser")
+var fetch = require('node-fetch');
+var domino = require('domino');
+
+module.exports =  async (req, res) => {
+  if(!req.query.videoId) {
+    res.json({
+      error: "Video ID not supplied"
+    });
+
+  } else if(!req.query.user) {
+    res.status(200).json({
+      error: "Username not supplied"
+    });
+  }else {
+    const video = `https://www.facebook.com/${req.query.user}/videos/${req.query.videoId}/`;
+    const resp = await fetch(video);
+    const html = await resp.text();
+    const doc = domino.createWindow(html).document;
+    const metadata = pageMetadataParser.getMetadata(doc, video);
+    const title  = metadata.title ? metadata.title : "Untitled";
+    const description = metadata.description ? metadata.description : "No Description";
+    const thumbnail = metadata.image;
+    const low = await fbvid.low(video);
+    const high = await fbvid.high(video);
+    var response = {}
+    if((typeof low) === "object") {
+      response.low = low;
+    } else {
+      response.low = {error: low};
+    }
+    if((typeof high) === "object") {
+      response.high = high;
+    } else {
+      response.high = {error: high};
+    }
+    if(!(response.low.error && response.high.error)) {
+      response.title = title;
+      response.description = description;
+      response.thumbnail = thumbnail;
+    }
+    res.json(response);
+  }
+};
---
+++ api/v2/ig.js
@@ -0,0 +1,33 @@
+const videoUrlLink = require("video-url-link")
+var pageMetadataParser = require("page-metadata-parser")
+var fetch = require('node-fetch');
+var domino = require('domino');
+
+
+module.exports = async (req, res) => {
+    if (!req.query.postId) {
+        res.json({
+            error: "Intagram Post Id not supplied"
+        });
+
+    } else {
+        const igURL = `https://www.instagram.com/p/${req.query.postId}`;
+        videoUrlLink.instagram.getInfo(igURL, (error, info) => {
+            if (error) {
+                if (error === {}) {
+                    res.json({ error: "Invalid Post ID" });
+                } else {
+                    res.json({ error: error });
+                }
+            } else {
+                const resp = await fetch(igURL);
+                const html = await resp.text();
+                const doc = domino.createWindow(html).document;
+                const metadata = pageMetadataParser.getMetadata(doc, igURL);
+                const title = metadata.title ? metadata.title : "Untitled";
+                const description = metadata.description ? metadata.description : "No Description";
+                res.json({title: title, description: description, media: info.list});
+            }
+        });
+    }
+};
